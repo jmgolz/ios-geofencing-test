fb3c782f24314095d15532dcd6fff91c9fad57b8
James Golz <jgolz@laserspineinstitute.com>
2016-02-24 15:12:21 -0500

    Adding in route detail screen. Will need to update the route selection list screen to pass the selected route data and coordinates to the route detail screen so that we can render the checkpoints (MKAnnotations) on the map.

---
+++ geofencingTest/SelectedRouteDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  SelectedRouteDetailViewController.m
+//  geofencingTest
+//
+//  Created by James Golz on 2/24/16.
+//  Copyright © 2016 Sharks with Laser Spines. All rights reserved.
+//
+
+#import "SelectedRouteDetailViewController.h"
+
+@interface SelectedRouteDetailViewController ()
+
+@end
+
+@implementation SelectedRouteDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

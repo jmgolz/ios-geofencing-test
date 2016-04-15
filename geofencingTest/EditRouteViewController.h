0de15eb20f7fcf9c43b19b64ade4e9c097eef0d4
James Golz <jgolz@laserspineinstitute.com>
2016-04-15 17:35:27 -0400

    Boilerplate for edit screen controller class and view

---
+++ geofencingTest/EditRouteViewController.h
@@ -0,0 +1,24 @@
+//
+//  EditRouteViewController.h
+//  geofencingTest
+//
+//  Created by James Golz on 4/15/16.
+//  Copyright © 2016 Sharks with Laser Spines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "RouteData.h"
+#import "RouteCoordinate.h"
+#import "RouteStorageManager.h"
+
+@interface EditRouteViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UITextField *authorTextField;
+@property (weak, nonatomic) IBOutlet UITextField *routeNameTextField;
+@property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+
+@property RouteStorageManager *routeStorageManager;
+
+@end
---
+++ geofencingTest/EditRouteViewController.m
@@ -0,0 +1,37 @@
+//
+//  EditRouteViewController.m
+//  geofencingTest
+//
+//  Created by James Golz on 4/15/16.
+//  Copyright © 2016 Sharks with Laser Spines. All rights reserved.
+//
+
+#import "EditRouteViewController.h"
+
+@interface EditRouteViewController ()
+
+@end
+
+@implementation EditRouteViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
